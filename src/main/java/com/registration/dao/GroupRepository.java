0e50944cab1cdc656b53b15e02f72a8451e47e46
alex <xalexpintax@gmail.com>
2016-07-09 19:44:57 +0300

    remove comment

--- src/main/java/com/registration/dao/GroupRepository.java
+++ src/main/java/com/registration/dao/GroupRepository.java
@@ -10,8 +10,6 @@ public interface GroupRepository extends CrudRepository<Group, Long> {
     Group findByGroupId(Long id);
     Group save(Group group);
 
-//    @Query(value = "select gr.* from temp_users_credentials as cr INNER JOIN " +
-//            "vk_group as gr ON cr.user_id = gr.user_id", nativeQuery = true) TODO need to get data
     @Query(value = "select gr.* from vk_group as gr", nativeQuery = true)
     List<Group> getActualGroups();
 
